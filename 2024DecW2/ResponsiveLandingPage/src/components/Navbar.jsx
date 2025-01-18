5d2829edf4d97a52f182bd701fda43b850cc7ab3
Gaurav Changmai <gchangmai007@gmail.com>
2025-01-18 16:36:00 +0530

    chai aur use context

--- 2024DecW2/ResponsiveLandingPage/src/components/Navbar.jsx
+++ 2024DecW2/ResponsiveLandingPage/src/components/Navbar.jsx
@@ -2,7 +2,7 @@ import React from 'react'
 
 const Navbar = () => {
   return (
-    <nav className="sticky py-4 top-0 z-50 background-blur-lg border-b border-sky-600/80">
+    <nav className="sticky py-4 top-0 z-50 background-blur-lg border-b border-neutral-600/80">
         <div className="container mx-auto px-3 text-sm">
             <div className="flex justify-center items-center">
                 <div>hi</div>
